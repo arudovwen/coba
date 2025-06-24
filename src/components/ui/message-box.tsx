627e888d56461317da4673e28eb3eea0e3ec2a17
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-06-24 09:30:39 +0100

    Update message-box.tsx

--- src/components/ui/message-box.tsx
+++ src/components/ui/message-box.tsx
@@ -1,15 +1,41 @@
+import { useState, useRef, useEffect } from "react";
 import SendSvg from "../pages/chats/builder/svgs/send";
 import UploadMenu from "./upload.menu";
 
 export default function MessageBox() {
+  const [value, setValue] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
+  };
+
+  const resizeTextArea = () => {
+    if (textareaRef.current) {
+      // Reset the height to auto before measuring the new scrollHeight
+      textareaRef.current.style.height = "auto";
+      // Set the new height based on the scrollHeight, and limit it to max height
+      textareaRef.current.style.height =
+        Math.min(textareaRef.current.scrollHeight, 200) + "px";
+    }
+  };
+
+  useEffect(() => {
+    resizeTextArea();
+  }, [value]);
   return (
-    <div className="rounded-3xl border border-[#D5D5D5] bg-[#F5F5FA] h-full px-4 py-3
-     message_shadow relative">
+    <div
+      className="rounded-3xl border border-[#D5D5D5] bg-[#F5F5FA] h-full px-4 py-3
+     message_shadow relative"
+    >
       <div className="flex w-full items-start gap-x-1 h-full">
         <span className="text-main font-semibold">@</span>
         <textarea
+          ref={textareaRef}
+          value={value}
+          onChange={handleChange}
           placeholder="add context"
-          className="outline-none flex-1 px-1 py-1 pr-4 rounded-lg text-sm max-w-[80%] resize-none max-h-[300px] h-full"
+          className="outline-none flex-1 px-1 py-1 pr-4 rounded-lg text-sm max-w-[80%] resize-none max-h-[200px] h-full"
         ></textarea>
       </div>
 
@@ -17,7 +43,7 @@ export default function MessageBox() {
         className="flex gap-x-3 items-center absolute right-2 bottom-2
       "
       >
-       <UploadMenu />
+        <UploadMenu />
         <button aria-label="attach" type="button">
           <SendSvg />
         </button>
